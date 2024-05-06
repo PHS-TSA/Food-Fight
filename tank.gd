f35cc17f36db5f18ede81924318a6677cabf0727
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-05-05 21:06:00 -0500

    Wonky changes
    kind of a merge thing
    THIS CODE IS CURRENTLY IN COMPLETE

--- tank.gd
+++ tank.gd
@@ -11,33 +11,16 @@ func _physics_process(delta):
 	
 	#need to make tank rotate smoothly according to direction,
 	var direction = Input.get_vector("left","right","up","down")
-	
 	velocity = direction * SPEED
+
+	#Rotates the tank based on direction. Have w/s move it forward / backward. Have a/d rotate the tank left and right
+	if(Input.is_action_pressed("up")):
+		print("work")
+		#velocity
 	
-	print(%TankBottomGreen.global_rotation)
-	match(Input):
-		"left":
-			%TankBottomGreen.global_rotation.move_toward(PI,rotation_rate)
-			%TankCollision.global_rotation.move_toward(PI,rotation_rate)
-			print(%TankBottomGreen.global_rotation)
-		"right":
-			%TankBottomGreen.global_rotation.move_toward(0,rotation_rate)
-			%TankCollision.global_rotation.move_toward(0,rotation_rate)
-			print(%TankBottomGreen.global_rotation)
-		"up":
-			%TankBottomGreen.global_rotation.move_toward(PI/2,rotation_rate)
-			%TankCollision.global_rotation.move_toward(PI,rotation_rate)
-		"down":
-			%TankBottomGreen.global_rotation.move_toward(3*PI/2,rotation_rate)
-			%TankCollision.global_rotation.move_toward(3*PI/2,rotation_rate)
-		
 	
-	#var rotate_value:float = 
 	
 	%TankTopGreen.global_rotation = %TankTopGreen.global_position.angle_to_point(get_global_mouse_position()) + (PI/2)
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 
 	move_and_slide()

eff21516892dfe1c7c437244a366f5a3916cd4f2
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-05-08 14:06:06 -0500

    added updated aim system

---
+++ bullet.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var velocity := Vector2.RIGHT
+var traveled_distance := 0.0
+var num: int
+
+@onready var ball: Sprite2D = %BallSprite
+
+
+func _physics_process(delta: float) -> void:
+	const SPEED := 300
+	const RANGE := 1200
+	self.position += self.velocity * SPEED * delta
+	self.traveled_distance += SPEED * delta
+	if self.traveled_distance > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	
--- tank.gd
+++ tank.gd
@@ -2,25 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var rotation_rate:float = 100
+var rotationSpeed:float = 4
+var aimSpeed:float = 5
+var aimMethod:bool = true #true = key board. false = mouse
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
+var rotationDirection = 0
 
-func _physics_process(delta):
+func get_input():
+	rotationDirection = Input.get_axis("rotate_left", "rotate_right") 
+	velocity = transform.x * Input.get_axis("backwards", "forwards") * SPEED
+	if(aimMethod):
+		if(Input.is_action_pressed("aim_left")):
+			%TankTopGreen.global_rotation -= deg_to_rad(aimSpeed)
+		if(Input.is_action_pressed("aim_right")):
+			%TankTopGreen.global_rotation += deg_to_rad(aimSpeed)
+	else:
+		%TankTopGreen.global_rotation = %TankTopGreen.global_position.angle_to_point(get_global_mouse_position()) + (PI/2)
 	
-	#need to make tank rotate smoothly according to direction,
-	var direction = Input.get_vector("left","right","up","down")
-	velocity = direction * SPEED
+func _physics_process(delta):	
+	get_input()
+	rotation += rotationDirection * rotationSpeed * delta
+	move_and_slide()
+	
 
-	#Rotates the tank based on direction. Have w/s move it forward / backward. Have a/d rotate the tank left and right
-	if(Input.is_action_pressed("up")):
-		print("work")
-		#velocity
 	
-	
-	
-	%TankTopGreen.global_rotation = %TankTopGreen.global_position.angle_to_point(get_global_mouse_position()) + (PI/2)
+
 	
 
 	move_and_slide()

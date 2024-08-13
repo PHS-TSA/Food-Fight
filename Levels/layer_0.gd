5358e7369ad9b6168ca6f8c2c5b7107d289d6d2b
Matthew Wasser <71449347+MattsAttack@users.noreply.github.com>
2024-08-12 23:33:49 -0500

    working path finding with obstacles

---
+++ Levels/layer_0.gd
@@ -0,0 +1,14 @@
+extends TileMapLayer
+
+@onready var obstacles:TileMapLayer = $"../Obstacles"
+
+
+func _use_tile_data_runtime_update(coords):
+	if coords in obstacles.get_used_cells_by_id(2): #ID 2 because the id for this tileset is 2
+		return true
+	return false
+
+func _tile_data_runtime_update(coords:Vector2i, tile_data:TileData):
+	if(coords in obstacles.get_used_cells_by_id(2)): #Checks if there obstacles on the coords for this layer
+		tile_data.set_navigation_polygon(0,null)
+		
